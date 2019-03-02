bf16fe00d69fbacb1aaf954c17552cae00ec3638
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-03-01 22:55:06 -0700

    updated style and java

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// alert( "Welcome to the crystals game!" );
+//Create variables needed
+var wins = 0;
+var losses = 0;
+var imagePoints = [12, 4, 7, 3];
+var randomScore = [];
+
+$( "#mo-salah" ).on( "click", function () {
+    alert( "You chose Mo Salah!" );
+} );
